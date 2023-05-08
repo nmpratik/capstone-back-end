be27215864a19f5e2584745f495ea6cf86685d04
nmpratik <nmprathik@gmail.com>
2023-05-08 15:51:59 +0530

    safe commit

---
+++ Event-Management-System/src/test/java/com/pra/ems/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pra.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
